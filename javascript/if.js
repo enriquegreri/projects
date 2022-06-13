bb92916205a97e283afd3a376887b35d6d1565dd
Enrique Gregori Ribera <enriquegreri@gmail.com>
2022-06-13 16:11:56 +0200

    if.js

---
+++ javascript/if.js
@@ -0,0 +1,21 @@
+var age = 18;
+if (age === 18) {
+    console.log("Puedes votar, será tu primera vez");
+} else if (age > 18) {
+    console.log("Puedes votar de nuevo");
+} else if (age > 18) {
+    console.log("Puedes votar de nuevo");
+} else if (age > 18) {
+    console.log("Puedes votar de nuevo");
+} else if (age > 18) {
+    console.log("Puedes votar de nuevo");
+} else {
+    console.log("Aun no puedes votar");
+};
+
+
+// condition ? true : false;
+
+var number = 1;
+var resultado = number === 1 ? "Si soy uno" : "No, no soy uno";
+console.log(resultado);
